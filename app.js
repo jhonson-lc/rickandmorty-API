c24f482d796d8c43e3e5e15ce23184f5d71b6ca4
jhonson-lc <jhoncisneros11@gmail.com>
2021-12-15 18:26:32 -0500

    API v.1.1

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const template_card = document.getElementById('template__card');
-const fragment = document.createDocumentFragment();
-const card = document.getElementById('cards');
-
-let arrayPersonajes = [];
+let pages = {};
 
 document.addEventListener('DOMContentLoaded', () => {
   dataPersonajes();
+});
+
+document.addEventListener('click', e => {
+  if (e.target.matches('#btn__prev')) {
+    if (pages.prev) {
+      dataPersonajes(pages.prev);
+    }
+  }
+  if (e.target.matches('#btn__next')) {
+    if (pages.next) {
+      dataPersonajes(pages.next);
+    }
+  }
 });
 
 const loadingData = state => {
@@ -17,12 +26,18 @@ const loadingData = state => {
   }
 };
 
-const dataPersonajes = async () => {
+const dataPersonajes = async (url = `https://rickandmortyapi.com/api/character/`) => {
   try {
     loadingData(true);
-    const res = await fetch(`https://rickandmortyapi.com/api/character/`);
+    const res = await fetch(url);
     const data = await res.json();
-    agregarPersonajes(data);
+    mostrarPersonajes(data);
+    mostrarBotones(data);
+
+    pages = {
+      prev: data.info.prev,
+      next: data.info.next,
+    };
   } catch (e) {
     console.log(e);
   } finally {
@@ -30,7 +45,11 @@ const dataPersonajes = async () => {
   }
 };
 
-const agregarPersonajes = data => {
+const mostrarPersonajes = data => {
+  const template_card = document.getElementById('template__card');
+  const fragment = document.createDocumentFragment();
+  const card = document.getElementById('cards');
+
   card.textContent = '';
   data.results.forEach(personaje => {
     const clone = template_card.content.cloneNode(true);
@@ -41,4 +60,26 @@ const agregarPersonajes = data => {
     fragment.appendChild(clone);
   });
   card.appendChild(fragment);
+};
+
+const mostrarBotones = data => {
+  const template_btn = document.getElementById('template__btn');
+  const btns = document.getElementById('btns');
+
+  btns.textContent = '';
+
+  const clone = template_btn.content.cloneNode(true);
+
+  if (data.info.prev) {
+    clone.getElementById('btn__prev').classList.remove('cursor-not-allowed');
+  } else {
+    clone.getElementById('btn__prev').classList.add('cursor-not-allowed');
+  }
+  if (data.info.next) {
+    clone.getElementById('btn__next').classList.remove('cursor-not-allowed');
+  } else {
+    clone.getElementById('btn__next').classList.add('cursor-not-allowed');
+  }
+
+  btns.appendChild(clone);
 };
